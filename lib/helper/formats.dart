f23526061e2d2919bfdfe69627502b2f40659ff5
Muhammad Irfan <nurburgring25@gmail.com>
2026-04-28 15:07:15 +0700

    Fixing bug

--- lib/helper/formats.dart
+++ lib/helper/formats.dart
@@ -1,4 +1,5 @@
 import "package:base/base.dart";
+import "package:dynamic_of_things/model/attachment.dart";
 import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
 import "package:jiffy/jiffy.dart";
@@ -123,6 +124,8 @@ class Formats {
           }
         } else if (value is Map<String, dynamic>) {
           convert(value);
+        } else if (value is Attachment) {
+          map[key] = null;
         }
       }
     });
