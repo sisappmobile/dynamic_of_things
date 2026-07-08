77e0f2bc1f5edaf82c77a18ffd1cfd916f61aa1d
Muhammad Irfan <nurburgring25@gmail.com>
2026-07-08 11:20:05 +0700

    Update

---
+++ ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -0,0 +1,58 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Generated file. Do not edit.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "FlutterGeneratedPluginSwiftPackage",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
+    ],
+    dependencies: [
+        .package(name: "camera_avfoundation", path: "../.packages/camera_avfoundation-0.9.23+2"),
+        .package(name: "connectivity_plus", path: "../.packages/connectivity_plus-7.1.1"),
+        .package(name: "device_info_plus", path: "../.packages/device_info_plus-12.4.0"),
+        .package(name: "file_picker", path: "../.packages/file_picker-10.1.9"),
+        .package(name: "geocoding_ios", path: "../.packages/geocoding_ios-3.1.0"),
+        .package(name: "geolocator_apple", path: "../.packages/geolocator_apple-2.3.13"),
+        .package(name: "image_picker_ios", path: "../.packages/image_picker_ios-0.8.13+6"),
+        .package(name: "mobile_scanner", path: "../.packages/mobile_scanner-7.2.0"),
+        .package(name: "native_device_orientation", path: "../.packages/native_device_orientation-2.1.0"),
+        .package(name: "package_info_plus", path: "../.packages/package_info_plus-9.0.1"),
+        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.6"),
+        .package(name: "sqflite_darwin", path: "../.packages/sqflite_darwin-2.4.2"),
+        .package(name: "syncfusion_flutter_pdfviewer", path: "../.packages/syncfusion_flutter_pdfviewer-33.2.7"),
+        .package(name: "url_launcher_ios", path: "../.packages/url_launcher_ios-6.4.1"),
+        .package(name: "video_player_avfoundation", path: "../.packages/video_player_avfoundation-2.9.7"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "camera-avfoundation", package: "camera_avfoundation"),
+                .product(name: "connectivity-plus", package: "connectivity_plus"),
+                .product(name: "device-info-plus", package: "device_info_plus"),
+                .product(name: "file-picker", package: "file_picker"),
+                .product(name: "geocoding-ios", package: "geocoding_ios"),
+                .product(name: "geolocator-apple", package: "geolocator_apple"),
+                .product(name: "image-picker-ios", package: "image_picker_ios"),
+                .product(name: "mobile-scanner", package: "mobile_scanner"),
+                .product(name: "native-device-orientation", package: "native_device_orientation"),
+                .product(name: "package-info-plus", package: "package_info_plus"),
+                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
+                .product(name: "sqflite-darwin", package: "sqflite_darwin"),
+                .product(name: "syncfusion-flutter-pdfviewer", package: "syncfusion_flutter_pdfviewer"),
+                .product(name: "url-launcher-ios", package: "url_launcher_ios"),
+                .product(name: "video-player-avfoundation", package: "video_player_avfoundation"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
+        )
+    ]
+)
